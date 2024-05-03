c85afca2c93f0e10c5a33ff81e800cc2e666bc99
EveAmmerman <ammerman.eve@gmail.com>
2024-05-03 16:32:59 +0200

    included batch file that allows jupyter notebooks to be excecuted directly from the windows file browser

---
+++ jupyter_launch_script.bat
@@ -0,0 +1,1 @@
+C:\Users\amsp\Anaconda3\python.exe C:\Users\amsp\Anaconda3\cwp.py C:\Users\amsp\Anaconda3 C:\Users\amsp\Anaconda3\python.exe C:\Users\amsp\Anaconda3\Scripts\jupyter-notebook-script.py %1
